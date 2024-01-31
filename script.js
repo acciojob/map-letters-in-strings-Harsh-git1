bda87b9eb04743ccfc360264d149275a8092e4d7
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-01-31 12:22:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let ans = {};
+	for(let i=0; i<str.length; i++) {
+		let ch = str[i];
+
+		if(!ans[ch]) {
+			ans[ch] = [];
+		}
+
+		ans[ch].push(i);
+	}
+
+	return ans;
+}
+
+const str = prompt("enter the string");
+alert(mapLetters(str));
+
